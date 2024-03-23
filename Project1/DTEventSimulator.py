7fe081a7d84dbf77b5cf49c19fc2ad63eea0fd72
Evan Smith <ers131@txstate.edu>
2024-03-22 22:54:53 -0500

    format

--- Project1/DTEventSimulator.py
+++ Project1/DTEventSimulator.py
@@ -1,4 +1,8 @@
 import random
+import math
+
+###################################################################cpu#####################################
+
 class CPU:
    def __init__(self):
       self.clock = 0
@@ -6,6 +10,7 @@ class CPU:
       self.current_process = Process()
       self.ready_queue = []
 
+###################################################################disk#####################################
 class Disk:
    def __init__(self):
       self.clock = 0
@@ -13,6 +18,7 @@ class Disk:
       self.current_process = Process()
       self.queue = []
 
+###################################################################process#####################################
 class Process:
    def __init__(self):
       self.arrival_time = 0
@@ -21,13 +27,14 @@ class Process:
       self.disk_service_time = 0
       self.cpu_done = False
       self.disk_done = False
-
+###################################################################event#####################################
 class Event:
    def __init__(self, time, type, process):
       self.time = time
       self.type = type
       self.process = process
 
+###################################################################simulator#####################################
 class Simulator:
    def __init__(self, average_arrival_rate, average_CPU_service_time, average_Disk_service_time, end_condition):
       self.average_arrival_rate = average_arrival_rate
@@ -37,15 +44,20 @@ class Simulator:
 
       self.clock = 0
       self.event_queue = []
-      self.tasks_processed = 0
-   
+      self.completed_processes = 0
+
    def first_come_first_serve(self):
       # Implement the FCFS 
       print("Running the simulation")
+      first_process = self.generateProcess()
 
 
-#process.arrival_time = self.cpu.clock + (math.log(1 - float(random.uniform(0, 1))) / (-self.lambda_val))
-#process.service_time = math.log(1 - float(random.uniform(0, 1))) / (-(1/self.avg_service_time))
-      
-def run(self):
-   self.first_come_first_serve()
+   def generateProcess(self):
+      process = Process()
+      process.arrival_time = self.clock + (math.log(1 - float(random.uniform(0, 1))) / (-self.average_arrival_rate))
+
+   #process.arrival_time = self.cpu.clock + (math.log(1 - float(random.uniform(0, 1))) / (-self.lambda_val))
+   #process.service_time = math.log(1 - float(random.uniform(0, 1))) / (-(1/self.avg_service_time))
+         
+   def run(self):
+      self.first_come_first_serve()
