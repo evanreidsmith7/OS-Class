2c29dbcf7deb7b5e24d5081cc884902887dd653f
Evan Smith <ers131@txstate.edu>
2024-03-22 21:48:56 -0500

    begin generating processes

--- Project1/DTEventSimulator.py
+++ Project1/DTEventSimulator.py
@@ -1,4 +1,32 @@
 import random
+class CPU:
+   def __init__(self):
+      self.clock = 0
+      self.busy = False
+      self.current_process = Process()
+      self.ready_queue = []
+
+class Disk:
+   def __init__(self):
+      self.clock = 0
+      self.busy = False
+      self.current_process = Process()
+      self.queue = []
+
+class Process:
+   def __init__(self):
+      self.arrival_time = 0
+      self.service_time = 0
+      self.cpu_service_time = 0
+      self.disk_service_time = 0
+      self.cpu_done = False
+      self.disk_done = False
+
+class Event:
+   def __init__(self, time, type, process):
+      self.time = time
+      self.type = type
+      self.process = process
 
 class Simulator:
    def __init__(self, average_arrival_rate, average_CPU_service_time, average_Disk_service_time, end_condition):
@@ -10,4 +38,14 @@ class Simulator:
       self.clock = 0
       self.event_queue = []
       self.tasks_processed = 0
+   
+   def first_come_first_serve(self):
+      # Implement the FCFS 
+      print("Running the simulation")
+
+
+#process.arrival_time = self.cpu.clock + (math.log(1 - float(random.uniform(0, 1))) / (-self.lambda_val))
+#process.service_time = math.log(1 - float(random.uniform(0, 1))) / (-(1/self.avg_service_time))
       
+def run(self):
+   self.first_come_first_serve()
