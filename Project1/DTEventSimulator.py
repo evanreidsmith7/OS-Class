8a9098c271a4d9dff91fdf21a072f47de580c17d
Evan Smith <ers131@txstate.edu>
2024-03-25 01:00:28 -0500

    updating metrics in arival

--- Project1/DTEventSimulator.py
+++ Project1/DTEventSimulator.py
@@ -58,25 +58,25 @@ class Simulator:
 #################################################first_come_first_serve#
    def first_come_first_serve(self):
       # Implement the FCFS 
-      print("Running the scheduler...") if self.debug else None
+      print("Running the scheduler...") if self.debug is True else None
       first_process = self.generateProcess()
-      print(f"First process generated with PID={first_process.pid} and arrival time={first_process.arrival_time}\n\n") if self.debug else None
+      print(f"First process generated with PID={first_process.pid} and arrival time={first_process.arrival_time}\n\n") if self.debug is True else None
 
       first_event = self.generateEvent(first_process.arrival_time, "ARR", first_process)
-      print(f"First event scheduled: Type={first_event.type}, Time={first_event.time}, Process PID={first_event.process.pid}\n\n") if self.debug else None
+      print(f"First event scheduled: Type={first_event.type}, Time={first_event.time}, Process PID={first_event.process.pid}\n\n") if self.debug is True else None
       self.event_queue.append(first_event)
 
       while self.number_completed_processes < self.end_condition:
-         print("\n\n\n\n") if self.debug else None
-         print(f"Current simulation clock: {self.cpu.clock}") if self.debug else None
-         print(f"Number of completed processes: {self.number_completed_processes}") if self.debug else None
-         print(f"Event queue size: {len(self.event_queue)}, Ready queue size: {len(self.ready_queue)}, Disk queue size: {len(self.disk_queue)}\n\n") if self.debug else None
+         print("\n\n\n\n") if self.debug is True else None
+         print(f"Current simulation clock: {self.cpu.clock}") if self.debug is True else None
+         print(f"Number of completed processes: {self.number_completed_processes}") if self.debug is True else None
+         print(f"Event queue size: {len(self.event_queue)}, Ready queue size: {len(self.ready_queue)}, Disk queue size: {len(self.disk_queue)}\n\n") if self.debug is True else None
          # sort the event queue so that the next occuring event appears
          self.event_queue.sort(key=lambda x: x.time)
 
          # take the next event from the event queue
          event = self.event_queue.pop(0)
-         print(f"Processing event: Type={event.type}, Time={event.time}, Process PID={event.process.pid}\n\n\n\n") if self.debug else None
+         print(f"Processing event: Type={event.type}, Time={event.time}, Process PID={event.process.pid}\n\n\n\n") if self.debug is True else None
 
 
          # set clock to the occuring event time because time hops around
@@ -93,22 +93,22 @@ class Simulator:
          elif event.type == "DEP":
             self.handleDeparture(event)
          else:
-            print("Invalid event type") if self.debug else None
+            print("Invalid event type") if self.debug is True else None
          
-         print(f"\n\nPost-event processing: Simulation clock: {self.cpu.clock}, Ready queue size: {len(self.ready_queue)}, Disk queue size: {len(self.disk_queue)}") if self.debug else None
+         print(f"\n\nPost-event processing: Simulation clock: {self.cpu.clock}, Ready queue size: {len(self.ready_queue)}, Disk queue size: {len(self.disk_queue)}") if self.debug is True else None
          avg_turn_around_time = (self.total_turnaround_time / self.end_condition)
          throughput = (self.end_condition / self.cpu.clock)
          cpu_utilization = 100 * (self.total_cpu_service_times / self.cpu.clock)
          disk_utilization = 100 * (self.total_disk_service_times / self.cpu.clock)
-         print(f"{'Metrics Report':^40}")
-         print(f"{'='*40}")
-         print(f"{'Throughput:':<30}{throughput:>10.4f} processes/unit time")      
-         print(f"{'CPU Utilization:':<30}{cpu_utilization:>10.4f}%")
-         print(f"{'Disk Utilization:':<30}{disk_utilization:>10.4f}%")
-         print(f"{'Avg. Turnaround Time:':<30}{avg_turn_around_time:>10.4f} seconds")
-         print(f"{'*average_cpu_service_time':<30}{self.total_cpu_service_times / self.end_condition:>10.4f} seconds")
-         print(f"{'*average_disk_service_time':<30}{self.total_disk_service_times / self.num_disk_processes:>10.4f} seconds")
-         print(f"{'='*40}")
+         print(f"{'Metrics Report':^40}") if self.debug is True else None
+         print(f"{'='*40}") if self.debug is True else None
+         print(f"{'Throughput:':<30}{throughput:>10.4f} processes/unit time") if self.debug is True else None      
+         print(f"{'CPU Utilization:':<30}{cpu_utilization:>10.4f}%") if self.debug is True else None
+         print(f"{'Disk Utilization:':<30}{disk_utilization:>10.4f}%") if self.debug is True else None
+         print(f"{'Avg. Turnaround Time:':<30}{avg_turn_around_time:>10.4f} seconds") if self.debug is True else None
+         print(f"{'*average_cpu_service_time':<30}{self.total_cpu_service_times / self.end_condition:>10.4f} seconds") if self.debug is True else None
+         print(f"{'*average_disk_service_time':<30}{self.total_disk_service_times / self.num_disk_processes:>10.4f} seconds") if self.debug is True else None
+         print(f"{'='*40}") if self.debug is True else None
 
 
 
@@ -123,27 +123,39 @@ class Simulator:
 
 ##########################################################handleArrival#
    def handleArrival(self, event):
-      print(f"Handling Arrival at time={self.cpu.clock}, Process ID={event.process.pid}, CPU Busy={self.cpu.busy}, Ready Queue Size={len(self.ready_queue)}\n\n") if self.debug else None
+      print(f"Handling Arrival at time={self.cpu.clock}, Process ID={event.process.pid}, CPU Busy={self.cpu.busy}, Ready Queue Size={len(self.ready_queue)}\n\n") if self.debug is True else None
       if self.cpu.busy is False:
          # cpu isnt busy and the process is not going to disk
          # start the process on the cpu (cpu.busy true) 
          self.cpu.busy = True
          disk_probability = float(random.uniform(0, 1))
          event_action = "departing" if disk_probability <= 0.6 else "going to disk"
-         event.type = "DEP" if disk_probability <= 0.6 else "DISK_ARR"
-         event.time = self.cpu.clock + event.process.cpu_service_time
-
-         print(f"Process {event_action} after CPU service. Scheduling {event.type} event at time={event.time}\n\n") if self.debug else None
-         self.event_queue.append(event)
+         #event.type = "DEP" if disk_probability <= 0.6 else "DISK_ARR"
+         #event.time = self.cpu.clock + event.process.cpu_service_time
+         if disk_probability <= 0.6:
+            # process is done with cpu service
+            self.total_cpu_service_times += event.process.cpu_service_time
+            event.type = "DEP"
+            event.time = self.cpu.clock + event.process.cpu_service_time
+            print(f"Process {event_action} after CPU service. Scheduling {event.type} event at time={event.time}\n\n") if self.debug is True else None
+            self.event_queue.append(event)
+         else:
+            # process is going to disk
+            self.total_cpu_service_times += event.process.cpu_service_time
+            event.type = "DISK_ARR"
+            event.time = self.cpu.clock
+            self.event_queue.append(event)
+            print(f"Process {event_action} after CPU service. Scheduling {event.type} event at time={event.time}\n\n") if self.debug is True else None
+            
 
       elif self.cpu.busy is True:
-         print("cpu is busy, Process going to ready queue\n\n") if self.debug else None
+         print("cpu is busy, Process going to ready queue\n\n") if self.debug is True else None
          # cpu is busy and the process is not going to disk
          # add the process to the ready queue
          self.ready_queue.append(event.process)
 
       else:
-         print("Something is wrong with the cpu\n\n") if self.debug else None
+         print("Something is wrong with the cpu\n\n") if self.debug is True else None
 
       # Log the state after handling the arrival
       print(f"Post-Arrival: CPU Busy={self.cpu.busy}, Ready Queue Size={len(self.ready_queue)}, Event Queue Size={len(self.event_queue)}\n\n") if self.debug else None
@@ -161,6 +173,8 @@ class Simulator:
    def handleDiskArival(self, event):
       print(f"Handling Disk Arrival at time={self.cpu.clock}, Process ID={event.process.pid}, Disk Busy={self.disk.busy}, Disk Queue Size={len(self.disk_queue)}") if self.debug else None
       self.cpu.busy = False
+      # update cpu metrics
+      self.total_cpu_service_times += event.process.cpu_service_time
       if self.disk.busy is False:
          # disk isnt busy
          print("disk is not busy, process Departing Disk after disk service") if self.debug else None
@@ -169,6 +183,7 @@ class Simulator:
          event.type = "DISK_DEP"
          # update the event time to when its done getting service with disk
          event.time = self.cpu.clock + event.process.disk_service_time
+         self.total_disk_service_times += event.process.disk_service_time
          print(f"Disk is not busy, scheduling {event.type} for Process ID={event.process.pid} at time={event.time}\n\n") if self.debug else None
          # add the event back to the event queue
          self.event_queue.append(event)
@@ -188,10 +203,11 @@ class Simulator:
       print(f"Handling Disk Departure at time={self.cpu.clock}, Process ID={event.process.pid}, Disk Busy={self.disk.busy}, Disk Queue Size={len(self.disk_queue)}\n\n") if self.debug else None
       # process is done with disk update metrics
       self.sum_num_of_proc_in_diskQ += len(self.disk_queue)
-      self.total_disk_service_times += event.process.disk_service_time
+      
 
       # since the process is done with disk, it is going back to the cpu
       self.ready_queue.append(event.process)
+      print("process appended to ready queue, ", event.process.pid) if self.debug else None
       '''
       # change the event to an arrival event since it is going to the cpu
       event.type = "ARR"
@@ -210,7 +226,8 @@ class Simulator:
          # pull the next process from the disk queue
          process_departing = self.disk_queue.pop(0)
          depart_time = self.cpu.clock + process_departing.disk_service_time
-
+         self.total_disk_service_times += process_departing.process.disk_service_time
+         
          new_departure_event = self.generateEvent(depart_time, "DISK_DEP", process_departing)#maybe##############################################################################################################
          self.event_queue.append(new_departure_event)
          print(f"Process ID={process_departing.pid} pulled from disk queue for service. Scheduling DISK_DEP event at time={depart_time}\n\n") if self.debug else None
@@ -227,16 +244,17 @@ class Simulator:
       self.sum_num_of_proc_in_readyQ += len(self.ready_queue)
       self.number_completed_processes += 1
       self.total_turnaround_time += turnaround_time
-      self.total_cpu_service_times += event.process.cpu_service_time
+      # self.total_cpu_service_times += event.process.cpu_service_time
       print(f"Process ID={event.process.pid} departed. Turnaround time: {turnaround_time}, CPU service time: {event.process.cpu_service_time}\n\n") if self.debug else None      
       
       # if ready queue is empty, cpu is idle
       if len(self.ready_queue) == 0:
          self.cpu.busy = False
-         print("Ready queue is empty. CPU set to idle.\n\n") if self.debug else None
+         print("Ready queue is empty. CPU set to idle.\n\n") if self.debug is True else None if self.debug else None
       else:
          # pull the next process from the ready queue
          process_departing = self.ready_queue.pop(0)
+         print("process departing", process_departing.pid) if self.debug else None
          depart_time = self.cpu.clock + process_departing.cpu_service_time
 
          print(f"Next process ID={process_departing.pid} pulled from ready queue. Scheduled departure at time={depart_time}\n\n") if self.debug else None
@@ -244,7 +262,7 @@ class Simulator:
          new_departure_event = self.generateEvent(depart_time, "DEP", process_departing)
          self.event_queue.append(new_departure_event)
       
-      print(f"Post-Departure: CPU Busy={self.cpu.busy}, Ready Queue Size={len(self.ready_queue)}, Completed Processes={self.number_completed_processes}\n\n\n\n") if self.debug else None
+      print(f"Post-Departure: CPU Busy={self.cpu.busy}, Ready Queue Size={len(self.ready_queue)}, Completed Processes={self.number_completed_processes}\n\n\n\n")
 
 #######################################################generateProcess#
    def generateProcess(self):
