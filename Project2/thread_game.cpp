cec713d90cc0209051311aadc5da92ca6e45cef3
Evan Smith <ers131@txstate.edu>
2024-04-21 18:28:21 -0500

    workspace configured

--- Project2/thread_game.cpp
+++ Project2/thread_game.cpp
@@ -2,3 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    pthread_t thread1, thread2;
+    int iret1, iret2;
+
+    
+    exit(0);
+}
